fdd28015db263345d4cc9dca806b1d50379cddd6
CaycedoJuan <juanjosecayced@gmail.com>
2022-11-15 21:05:38 -0500

    push JJ

---
+++ Index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Relojeria Deluxe prototipo2</title>
+
+    <!-- CSS de Bootstrap -->
+    <link rel="stylesheet" href="css/index.css">
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <!-- librerías opcionales que activan el soporte de HTML5 para IE8 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <?php
+      include('header.php');
+    ?>
+
+      <!-- loader  -->
+      <!-- <div class="loader_bg">
+          <div class="loader"><img src="imgs/loading.gif" alt="" /></div>
+      </div>
+      <div class="wrapper"> -->
+          <!-- end loader -->
+
+          <!-- <div id="content"> -->
+
+
+              <!-- Bootstrap core JavaScript
+              ================================================== -->
+              <!-- Placed at the end of the document so the pages load faster -->
+              <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+              <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+              <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+              <script src="js/bootstrap/bootstrap.min.js"></script>
+
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!-- header -->
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="full">
+                    <a class="logo" href="index.html"><img class="img_logo" src="imgs/logo.png" alt="#" /></a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="full">
+                  <input type="text" class="buscar" placeholder="barra de busqueda"><i class='bx bx-search search'></i>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="full">
+                    <div class="right_header_info">
+                        <button id="buttonColor"><a class="button"  href="paginas/registro.html"> Registro</a><a class="button" >login</a></button>
+
+
+                        <ul>
+                            <li class="dinone"><img style="margin-right: 15px;margin-left: 15px;" src="imgs/icono_corazon.png" alt="#"><a href="#"></a></li>
+                            <li class="dinone"><img style="margin-right: 15px; position:relative; top:-2px;"  src="imgs/icono_user.png" alt="#"><a href="#"></a></li>
+
+                            <li class="dinone"><img style="margin-right: 15px;" src="imgs/icono_carro.png" alt="#"></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- end header -->
