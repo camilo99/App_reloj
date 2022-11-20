df3a74de26772cd10f9a5b020b544f4570415ce0
CaycedoJuan <juanjosecayced@gmail.com>
2022-11-20 11:06:49 -0500

    Cambios4

--- paginas/indexPerfil/usuario.php
+++ paginas/indexPerfil/usuario.php
@@ -1,29 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+
+        <!-- CSS de Bootstrap -->
+
+    <!-- Latest compiled and minified CSS -->
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="../../css/fontawesome.css">
+    <link rel="stylesheet" href="../../css/fontawesome.min.css">
+
+      <link rel="stylesheet" href="../../css/index.css">
+      <link rel="stylesheet" href="../../css/popup.css">
+
+
+</head>
+<body>
+    
 <header>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <div class="full">
-                    <a class="logo" href="paginas/registro.php"><img class="img_logo" src="imgs/logo.png" alt="#" /></a>
+                    <a class="logo" href="paginas/registro.php"><img class="imgLogo" src="../../imgs/logo.png" alt="#" /></a>
                 </div>
             </div>
-            
-            <div class="col-md-3">
-                <div class="full">
-                    <div class="right_header_info">
-                        <button id="buttonColor"><a class="button"  href="paginas/registro.php"> Iniciar Sesión  /   </a><a class="button" >   Registrarse</a></button>
+                        
+                    <div class="col-md-6">
+                        <ul id="userNav">
+                        <li class="dinone"><img style="margin-right: 15px; position:relative; top:-2px;"  src="../../imgs/icono_user.png" alt="#"><a href="#"></a></li>
 
+                        <li class="dinone"><img style="margin-right: 15px;" src="../../imgs/icono_carro.png" alt="#"></li>
 
-                        <ul id="ulIcons">
-                            <li class="dinone"><img style="margin-right: 15px;margin-left: 15px;" src="imgs/icono_corazon.png" alt="#"><a href="#"></a></li>
-                            <li class="dinone"><img style="margin-right: 15px; position:relative; top:-2px;"  src="imgs/icono_user.png" alt="#"><a href="#"></a></li>
-
-                            <li class="dinone"><img style="margin-right: 15px;" src="imgs/icono_carro.png" alt="#"></li>
-
-                        </ul>
+                        </ul>   
                     </div>
-                </div>
-            </div>
+        </div>
+    </div>
+            
         </div>
     </div>
 </header>
 <hr id="hr">
+
+<!-- Bootstrap core JavaScript
+     -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="../../js/fontawesome.js"></script>
+
+    <script src="../../js/bootstrap/bootstrap.min.js"></script>
+    <script src="../../js/popup.js"></script>
+
+</body>
+</html>
