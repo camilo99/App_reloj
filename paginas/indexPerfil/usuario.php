7b28de2565db83525812ee394c76db50a448a274
camilo99 <juancamilomojicamoreno@gmail.com>
2022-11-20 11:23:47 -0500

    papap

--- paginas/indexPerfil/usuario.php
+++ paginas/indexPerfil/usuario.php
@@ -21,7 +21,7 @@
 
 </head>
 <body>
-    
+
 <header>
     <div class="container-fluid">
         <div class="row">
@@ -30,18 +30,18 @@
                     <a class="logo" href="paginas/registro.php"><img class="imgLogo" src="../../imgs/logo.png" alt="#" /></a>
                 </div>
             </div>
-                        
+
                     <div class="col-md-6">
                         <ul id="userNav">
                         <li class="dinone"><img style="margin-right: 15px; position:relative; top:-2px;"  src="../../imgs/icono_user.png" alt="#"><a href="#"></a></li>
 
                         <li class="dinone"><img style="margin-right: 15px;" src="../../imgs/icono_carro.png" alt="#"></li>
 
-                        </ul>   
+                        </ul>
                     </div>
         </div>
     </div>
-            
+
         </div>
     </div>
 </header>
